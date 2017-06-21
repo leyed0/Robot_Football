e75ba50abb4dc95db665e77a7814f98245897a7f
Orley Eduardo Schimerski dos Santos <orley1994@hotmail.com>
2017-06-20 22:44:17 -0300

    basic project structure

---
+++ Arduino_Receiver/Arduino_Receiver.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		Arduino_Receiver.ino
+ Created:	6/20/2017 10:24:46 PM
+ Author:	orley
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
---
+++ Arduino_sender/Arduino_sender.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		Arduino_sender.ino
+ Created:	6/20/2017 10:24:24 PM
+ Author:	orley
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
